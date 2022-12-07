57be3e3f3ac9b121935f746d26db62931cb988bf
wushihao <511940029@qq.com>
2022-12-07 17:03:27 +0800

    test

--- shell/test_code_check_pre_commit.sh
+++ shell/test_code_check_pre_commit.sh
@@ -7,7 +7,7 @@ for FILE in $(git diff --name-only --cached); do
     fi
 
 
-    grep '///' $FILE 2>&1 >/dev/null
+    egrep '\/\/\/\(\.\*\)testCode' $FILE 2>&1 >/dev/null
     if [ $? -eq 0 ]; then
         # 将错误输出
         echo -e $FILE '文件中包含了TODO、debugger、alert其中一个关键字请删除后再提交'
