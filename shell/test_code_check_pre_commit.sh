5fa02cc02285c19fad033cf6a9916e3df81bd2b6
wushihao <511940029@qq.com>
2022-12-07 15:27:30 +0800

    test

--- shell/test_code_check_pre_commit.sh
+++ shell/test_code_check_pre_commit.sh
@@ -13,7 +13,7 @@ for FILE in `git diff --name-only --cached`; do
     # fi
 
     # 匹配不能上传的关键字
-    grep 'TestCode | ttt' --ignore-case $FILE 2>&1 >/dev/null
+    grep 'TestCode \| ttt' --ignore-case $FILE 2>&1 >/dev/null
     if [ $? -eq 0 ]; then
         # 将错误输出
         echo -e $FILE '文件中包含了TODO、debugger、alert其中一个关键字请删除后再提交'
