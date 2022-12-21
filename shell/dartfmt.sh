89cc2e250e46e59b8d95c5cf001141de59bf70dc
wushihao <511940029@qq.com>
2022-12-21 17:24:03 +0800

    test

--- shell/dartfmt.sh
+++ shell/dartfmt.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
-dart_files=$(git diff --cached --name-only --diff-filter=ACM | grep '.dart$')
 
-[ -z "$dart_files" ] && exit 0
+DARTFMT_OUTPUT=`dartfmt -w . | grep Formatted`
 
-function checkFmt() {
-  unformatted=$(dartfmt -n $dart_files)
-  [ -z "$unformatted" ] && return 0
+if [ -n "$DARTFMT_OUTPUT" ]; then
+  echo $DARTFMT_OUTPUT
+  echo "Re-attempt commit."
+  exit 1
+else
+  echo "All Dart files formatted correctly. Yay!"
+  exit 0
 
-  echo >&2 "Dart files must be formatted with dartfmt. Please run:"
-  for fn in $unformatted; do
-    echo >&2 "  dartfmt -w $PWD/$fn"
-  done
-
-  return 1
-}
-
-checkFmt || fail=yes
-
-[ -z "$fail" ] || exit 1
-
-echo 'all okay?'
-
-exit 0
+fi
