1f53580a888375c7854e0f459d8f02ca19cf5ff5
wushh <wush@flatincbr.com>
2023-04-10 14:36:13 +0800

    test

--- shell/dartfmt_pre_commit.sh
+++ shell/dartfmt_pre_commit.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-DARTFMT_OUTPUT=`dartfmt -w . | grep Formatted`
+name=""
+for FILE in $(git diff --name-only --cached); do
+    # 忽略检查的文件
+    if [[ $FILE == *".sh"* ]] ; then
+        continue
+    elif [[ $FILE == *".md"* ]] ; then
+        continue
+    fi
+
+    name="$name $FILE"
+done
+
+DARTFMT_OUTPUT=$(dartfmt -w name. | grep Formatted)
 
 if [ -n "$DARTFMT_OUTPUT" ]; then
-  echo $DARTFMT_OUTPUT
+  echo "$DARTFMT_OUTPUT"
   echo "Re-attempt commit."
   exit 1
 else
