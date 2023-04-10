82674957676d7744d3f5e24cc7872b8b3fadb8b9
wushh <wush@flatincbr.com>
2023-04-10 14:33:54 +0800

    test

--- shell/dartfmt_pre_commit.sh
+++ shell/dartfmt_pre_commit.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-echo "dartfmt_pre_commit.sh 运行了"
-name=""
-for FILE in $(git diff --name-only --cached); do
-    # 忽略检查的文件
-    if [[ $FILE == *".sh"* ]] ; then
-        continue
-    elif [[ $FILE == *".md"* ]] ; then
-        continue
-    fi
 
-    name="$name $FILE"
-done
-cd ..
-exec dartfmt -w "$name"
-exit
+DARTFMT_OUTPUT=`dartfmt -w . | grep Formatted`
+
+if [ -n "$DARTFMT_OUTPUT" ]; then
+  echo $DARTFMT_OUTPUT
+  echo "Re-attempt commit."
+  exit 1
+else
+  echo "All Dart files formatted correctly. Yay!"
+  exit 0
+
+fi
