1ec124af7064a857356140457a2021752f2a1eb2
wushh <wush@flatincbr.com>
2023-04-10 15:46:22 +0800

    完善脚本

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    ///
     return MaterialApp(
       title: 'Flutter Demo testcode',
-
       theme: ThemeData(
         // This is the theme of your application.
         //
